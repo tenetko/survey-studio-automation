1f2a4c16c579f64f2fa609f31eee268b0e9f1ba9
Maria Tenetko <Maria.Tenetko@ipsos-russia.ru>
2025-11-25 21:55:08 +0300

    create load arrow doc pharma

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-poetry run python .\src\tasks\get_operator_work_time.py 830125a303ed414db040614fb50c0ddb 23-071675-22-C 2> nul
+poetry run python .\src\tasks\get_operator_work_time.py 830125a303ed414db040614fb50c0ddb 23-071675-23-C 2> nul
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 53699
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 53770
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 53771
@@ -8,8 +8,10 @@ poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54416
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54582
 poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54845
-poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54877
-poetry run python .\src\tasks\get_calls_groups.py 830125a303ed414db040614fb50c0ddb 54877
-poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54878
-poetry run python .\src\tasks\get_calls_groups.py 830125a303ed414db040614fb50c0ddb 54878
-poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 54880
+poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 55949
+poetry run python .\src\tasks\get_calls_groups.py 830125a303ed414db040614fb50c0ddb 55949
+poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 55950
+poetry run python .\src\tasks\get_calls_groups.py 830125a303ed414db040614fb50c0ddb 55950
+poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 55951
+poetry run python .\src\tasks\get_outgoing_calls.py 830125a303ed414db040614fb50c0ddb 55952
+
